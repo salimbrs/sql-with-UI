0801ea248655f9644bdd2f5604a330f7364d94b0
salimbrs <salim.baris90@gmail.com>
2025-02-26 17:22:53 +0300

    feat: new project

---
+++ part1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace part1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ViewGrid_bt = new System.Windows.Forms.Button();
+            this.Insert_bt = new System.Windows.Forms.Button();
+            this.update_bt = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.RegNo_tx = new System.Windows.Forms.TextBox();
+            this.Name_tx = new System.Windows.Forms.TextBox();
+            this.Adress_tx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-6, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(804, 236);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ViewGrid_bt
+            // 
+            this.ViewGrid_bt.Location = new System.Drawing.Point(26, 370);
+            this.ViewGrid_bt.Name = "ViewGrid_bt";
+            this.ViewGrid_bt.Size = new System.Drawing.Size(75, 23);
+            this.ViewGrid_bt.TabIndex = 1;
+            this.ViewGrid_bt.Text = "View";
+            this.ViewGrid_bt.UseVisualStyleBackColor = true;
+            this.ViewGrid_bt.Click += new System.EventHandler(this.ViewGrid_bt_Click);
+            // 
+            // Insert_bt
+            // 
+            this.Insert_bt.Location = new System.Drawing.Point(144, 370);
+            this.Insert_bt.Name = "Insert_bt";
+            this.Insert_bt.Size = new System.Drawing.Size(75, 23);
+            this.Insert_bt.TabIndex = 2;
+            this.Insert_bt.Text = "Insert";
+            this.Insert_bt.UseVisualStyleBackColor = true;
+            this.Insert_bt.Click += new System.EventHandler(this.Insert_bt_Click);
+            // 
+            // update_bt
+            // 
+            this.update_bt.Location = new System.Drawing.Point(272, 370);
+            this.update_bt.Name = "update_bt";
+            this.update_bt.Size = new System.Drawing.Size(75, 23);
+            this.update_bt.TabIndex = 3;
+            this.update_bt.Text = "Update";
+            this.update_bt.UseVisualStyleBackColor = true;
+            this.update_bt.Click += new System.EventHandler(this.update_bt_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(438, 370);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 4;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(605, 370);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "View";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // RegNo_tx
+            // 
+            this.RegNo_tx.Location = new System.Drawing.Point(26, 308);
+            this.RegNo_tx.Name = "RegNo_tx";
+            this.RegNo_tx.Size = new System.Drawing.Size(100, 20);
+            this.RegNo_tx.TabIndex = 6;
+            // 
+            // Name_tx
+            // 
+            this.Name_tx.Location = new System.Drawing.Point(200, 308);
+            this.Name_tx.Name = "Name_tx";
+            this.Name_tx.Size = new System.Drawing.Size(100, 20);
+            this.Name_tx.TabIndex = 7;
+            // 
+            // Adress_tx
+            // 
+            this.Adress_tx.Location = new System.Drawing.Point(388, 308);
+            this.Adress_tx.Name = "Adress_tx";
+            this.Adress_tx.Size = new System.Drawing.Size(100, 20);
+            this.Adress_tx.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Reg No.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(409, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Adress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Adress_tx);
+            this.Controls.Add(this.Name_tx);
+            this.Controls.Add(this.RegNo_tx);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.update_bt);
+            this.Controls.Add(this.Insert_bt);
+            this.Controls.Add(this.ViewGrid_bt);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ViewGrid_bt;
+        private System.Windows.Forms.Button Insert_bt;
+        private System.Windows.Forms.Button update_bt;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox RegNo_tx;
+        private System.Windows.Forms.TextBox Name_tx;
+        private System.Windows.Forms.TextBox Adress_tx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ part1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace part1
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection con = new MySqlConnection("Server=localhost;Database=studentınformation;Uid=root;Pwd=salim1990;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewGrid_bt_Click(object sender, EventArgs e)  // view butonu
+        {
+
+            ViewDataGrid();
+
+        }
+
+        private void ViewDataGrid()
+        {
+
+
+            try
+            {
+                using (MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM student", con))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veriler yüklenirken hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void Insert_bt_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string query = "INSERT INTO student (RegNo, student, adress) VALUES (@RegNo, @Student, @Adress)";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@RegNo", RegNo_tx.Text);
+                cmd.Parameters.AddWithValue("@Student", Name_tx.Text);
+                cmd.Parameters.AddWithValue("@Adress", Adress_tx.Text);
+
+                con.Open();  // Bağlantıyı aç
+                cmd.ExecuteNonQuery();  // SQL komutunu çalıştır
+                ViewDataGrid();
+                ClearTextBox();
+                con.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Kayıt eklenirken hata oluştu: " + ex.Message);
+            }
+        }
+
+        void ClearTextBox()
+        {
+            RegNo_tx.Text = "";
+            Name_tx.Text = "";
+            // or use this one
+            Adress_tx.Clear();
+        }
+
+        private void update_bt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Lütfen güncellenecek bir satır seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int selectedId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                string query = "UPDATE student SET student = @Student, adress = @Adress, RegNo = @RegNo WHERE id = @id"; // updated acording to id
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", selectedId);
+                cmd.Parameters.AddWithValue("@RegNo", RegNo_tx.Text);   
+                cmd.Parameters.AddWithValue("@Student", Name_tx.Text);
+                cmd.Parameters.AddWithValue("@Adress", Adress_tx.Text);
+
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Kayıt başarıyla güncellendi!");
+                    ViewDataGrid();
+                    ClearTextBox();
+                }
+                else
+                {
+                    MessageBox.Show("Güncellenecek kayıt bulunamadı!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Güncelleme sırasında hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows.Count > 0) // Satır seçilmiş mi kontrol et
+                {
+                    int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+
+                    string query = "DELETE FROM student WHERE id = @id";
+                    using (MySqlCommand cmd = new MySqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        con.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        con.Close();
+
+                        if (result > 0)
+                        {
+                            MessageBox.Show("✅ Kayıt başarıyla silindi!");
+                            ViewDataGrid(); // Silme işleminden sonra tabloyu yenile
+                        }
+                        else
+                        {
+                            MessageBox.Show("⚠️ Kayıt bulunamadı!");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("⚠️ Lütfen silmek için bir satır seçin!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Hata: " + ex.Message, "Silme Başarısız");
+            }
+        }
+
+    }
+}
